f4e4962fe267ba8576d84e54191fd28d457f2bd6
Andrea Clement <samuel.dietz@epfl.ch>
2022-05-29 23:33:57 +0200

    working first draft of cut_enum

--- cut_enum.rs
+++ cut_enum.rs
@@ -13,20 +13,22 @@ fn main() {
     let NTK            = convert_to_table(contents.split('\n').collect());
     let (PI, GATES)    = find_PI_and_GATES(&NTK);
 
-    for node in NTK {
+    for node in &NTK {
         println!("{:?}", node);
     }
-    println!("\n");
 
     println!("{:?}", PI);
     println!("{:?}", GATES);
+    println!("\n");
+
+    build_cuts(&NTK, &PI, &GATES, 5);
 
 }
 
-fn convert_to_table(split: Vec<&str>) -> Vec<Vec<i64>> {
+fn convert_to_table(split: Vec<&str>) -> Vec<Vec<u16>> {
     let mut ntk = Vec::with_capacity(split.len());
     for i in 0..(split.len() - 1) {
-        let mut fanins: Vec<i64> = Vec::new();
+        let mut fanins: Vec<u16> = Vec::new();
         for elem in split[i].split(',') {
             if elem.len() > 0 {
                 fanins.push(elem.parse().unwrap());
@@ -39,17 +41,111 @@ fn convert_to_table(split: Vec<&str>) -> Vec<Vec<i64>> {
     return ntk;
 }
 
-fn find_PI_and_GATES(NTK: &Vec<Vec<i64>>) -> (Vec<i64>, Vec<i64>) {
+fn find_PI_and_GATES(NTK: &Vec<Vec<u16>>) -> (Vec<u16>, Vec<u16>) {
     let mut primary_inputs = Vec::new();
     let mut gates          = Vec::new();
 
     for i in 0..(NTK.len() - 1) {
         if NTK[i].is_empty() {
-            primary_inputs.push(i as i64);
+            primary_inputs.push(i as u16);
         } else {
-            gates.push(i as i64);
+            gates.push(i as u16);
         }
     }
 
     return (primary_inputs, gates);
+}
+
+fn dominates(cut1: &Vec<u16>, cut2: &Vec<u16>) -> bool {
+    if cut1.len() >= cut2.len() {
+        for i in cut2 {
+            if !(cut1.contains(&i)) {
+                return false;
+            }
+        }
+        return true;
+    }
+    return false;
+}
+
+
+fn is_dominated(cut: &Vec<u16>, set_of_cuts: &Vec<Vec<u16>>) -> bool {
+    for cut1 in set_of_cuts {
+        if dominates(cut1, cut) {
+            return true;
+        }
+    }
+    return false;
+}
+
+fn add_and_remove_dominated(cut: &Vec<u16>, set_of_cuts: &mut Vec<Vec<u16>>) {
+    let mut new_set = Vec::new();
+    new_set.push(cut.clone());
+    for i in 0..set_of_cuts.len() {
+        if !dominates(cut, &set_of_cuts[i]) {
+            new_set.push(set_of_cuts[i].clone());
+        }
+    }
+    *set_of_cuts = new_set.clone();
+}
+
+fn union(a: &Vec<u16>, b: &Vec<u16>) -> Vec<u16> {
+    let mut ret = a.clone();
+    for i in b {
+        if !ret.contains(&i) {
+            ret.push(*i);
+        }
+    }
+
+    return ret;
+}
+
+fn build_cuts(NTK: &Vec<Vec<u16>>, PI: &Vec<u16>, GATES: &Vec<u16>, k: usize) {
+    let mut CUTS: Vec<Vec<Vec<u16>>> = vec![Vec::new(); NTK.len()];
+
+    struct Rec<'s> {
+        f: &'s dyn Fn(&Rec, usize, usize, Vec<u16>, usize, &mut  Vec<Vec<Vec<u16>>>)
+    }
+
+    let rec = Rec {
+        f: &|rec: &Rec, nmb, end, curr: Vec<u16>, index, cuts| {
+            if nmb == end {
+                if curr.len() <= k && !is_dominated(&curr, &cuts[index]) {
+                    add_and_remove_dominated(&curr, &mut cuts[index]);
+                }
+            } else {
+                let step: Vec<u16> = NTK[index].clone();
+                let fanin: usize = usize::from(step[nmb]);
+                let fanin_cuts = cuts[fanin].clone();
+                for cut in fanin_cuts {
+                    (rec.f)(&rec, nmb + 1, end, union(&curr, &cut), index, cuts);
+                }
+            }
+        }
+    };
+
+    for i in PI {
+        let j = usize::from(*i);
+        let mut init = Vec::new();
+        init.push(*i);
+        CUTS[j].push(init.clone());
+    }
+
+    for i in GATES {
+        let j = usize::from(*i);
+
+        (rec.f)(&rec, 0, NTK[j].len(), Vec::new(), j, &mut CUTS);
+
+        let mut me: Vec<u16> = Vec::new();
+        me.push(*i);
+        CUTS[j].push(me.clone());
+    }
+
+    println!("CUTS :");
+    for set in CUTS {
+        for cut in set {
+            println!("{:?}", cut);
+        }
+        println!("");
+    }
 }
